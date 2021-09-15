83508ba423c43d72f9dadb1fe06602c937666c6b
alexandre1202 <alexandre1202@gmail.com>
2021-09-15 10:25:11 -0300

    Adding unit tests to get 100% coverage

--- src/test/java/br/com/aab/strategy/TaxGeneratorTest.java
+++ src/test/java/br/com/aab/strategy/TaxGeneratorTest.java
@@ -8,9 +8,16 @@ public class TaxGeneratorTest {
     private TaxGenerator taxGenerator = new TaxGenerator();
 
     @Test
-    public void testTaxGenerator() {
+    public void testTaxGeneratorISS() {
         Double result = taxGenerator.calcTax(1000D, TaxType.ISS);
         Double expected = 20D;
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void testTaxGeneratorSimplesNacional() {
+        Double result = taxGenerator.calcTax(1000D, TaxType.SIMPLESNACIONAL);
+        Double expected = 40D;
         Assertions.assertThat(result).isEqualTo(expected);
     }
 }
